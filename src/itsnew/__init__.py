e6ca0dce422592e1547c5c1fbef666924e7f29d5
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-10 19:37:38 -0700

    Boilerplate

---
+++ src/itsnew/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+from . import main2
+
+
+def main() -> int:
+    out = main2.render_template("extended.j2")
+    sys.stdout.write(out)
+    return 0
---
+++ src/itsnew/__main__.py
@@ -0,0 +1,5 @@
+import sys
+
+import itsnew
+
+sys.exit(itsnew.main())
